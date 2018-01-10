e6388ce9ddb4691e5383a5fe7196b20f14931993
Shuz <shuzvvn@gmail.com>
2018-01-11 00:10:38 +0800

    Update SNP_position.1.py

--- SNP_position.1.py
+++ SNP_position.1.py
@@ -2,7 +2,13 @@
 
 import sys, getopt, re
 
-# Usage: python3 /home/shutingcho/pyscript/change_annotation.1.py --in_list=/home/shutingcho/project/phyto30/phyto30.14/run3/effector/run3.list.2 --in_list_index=0 --in_info=/home/shutingcho/project/phyto30/phyto30.12/info/PLY_v1.cds.03.info.ko.cog.desc.merg --in_info_index=0 --match_pattern=hypothetical protein --match_index=8 --change_to=putative effector --out_info=/home/shutingcho/project/phyto30/phyto30.12/info/PLY_v1.cds.03.info.ko.cog.desc.merg.1
+# Usage: python3 /home/shutingcho/pyscript/SNP_position.1.py \
+# --in_list=/home/shutingcho/project/phyto39/phyto39.03/SNP.2.list \
+# --in_list_index=0 \
+# --cds_info=/scratch/shutingcho/phyto39/phyto39.03/source_data/cds/NC_007716.info \
+# --pseudo_info=/scratch/shutingcho/phyto39/phyto39.03/source_data/rRNA/NC_007716.rRNA \
+# --other_info=/scratch/shutingcho/phyto39/phyto39.03/source_data/tRNA/NC_007716.tRNA \
+# --out_file=/home/shutingcho/project/phyto39/phyto39.03/SNP.2.out
 
 opts, args = getopt.getopt(sys.argv[1:], '', longopts=[
 	'in_list=', 
@@ -30,61 +36,80 @@ for opt, arg in opts:
 		assert False, "unhandled option"
 
 # make function to get list
-def get_list_from_table(in_table, in_index = 0, get_range = False):
+def get_list_from_table(in_file, in_index = 0):
 	out_list = []
 	in_index = int(in_index)
-	with open(in_table, 'r') as in_table_h:
-		for line in in_table_h:
+	with open(in_file, 'r') as in_file_h:
+		for line in in_file_h:
 			words = line.strip('\n').split('\t')
-			if get_range:
-				out_list.append({
-					locus_tag : words[0], 
-					feature_range : range(int(words[3]), int(words[4])+1),
-					feature_name : ': '.join(words[7:9])})
-			else:
-				out_list.append(words[in_index])
+			out_list.append(words[in_index])
 	return out_list
+
+def get_range_from_info(in_file, start_index, name_index):
+	out_dict = {}
+	with open(in_file, 'r') as in_file_h:
+		for line in in_file_h:
+			words = line.strip('\n').split('\t')
+			out_dict[range(int(words[start_index]), int(words[start_index+1])+1)] = [words[0], words[name_index], words[name_index+1]]
+	return out_dict
 
 # get list of SNP
 snp_list = get_list_from_table(in_list, in_list_index)
+init_snp_list = get_list_from_table(in_list, in_list_index)
 
 # get list of feature ranges
+feature_lists = []
 if cds_info:
-	cds_range_list = get_list_from_table(cds_info, get_range = True)
+	cds_ranges = get_range_from_info(cds_info, 3, 7)
+	feature_lists.append(cds_ranges)
 if pseudo_info:
-	pseudo_range_list = get_list_from_table(pseudo_info, get_range = True)
+	pseudo_ranges = get_range_from_info(pseudo_info, 3, 7)
+	feature_lists.append(pseudo_ranges)
 if other_info:
-	other_range_list = get_list_from_table(other_info, get_range = True)
+	other_ranges = get_range_from_info(other_info, 1, 4)
+	feature_lists.append(other_ranges)
+
+# store result in a dict
+out_dict = {}
+
+# store count for SNP in each feature
+count_cds = 0
+count_pseudo = 0
+count_others = 0
+
+# intragenic SNP
+for i in snp_list: # each SNP position
+	i = int(i)
+	for feature_ranges in feature_lists: # each info dict
+		for feature_range in feature_ranges: # each range in dict
+			if i in feature_range:
+				if feature_ranges == cds_ranges:
+					feature_type = 'cds'
+					count_cds += 1
+				elif feature_ranges == pseudo_info:
+					feature_type = 'pseudo'
+					count_pseudo += 1
+				else:
+					feature_type = 'others'
+					count_others += 1
+				out_dict[i] = [feature_type] + feature_ranges[feature_range]
+				snp_list.remove(str(i))
+
+# intergenic SNP
+if snp_list:
+	count_intergenic = len(snp_list)
+	feature_type = 'intergenic'
+	for i in snp_list:
+		i = int(i)
+		out_dict[i] = [feature_type]
 
 # write output
 out_file_h = open(out_file, 'w')
-
-for i in snp_list:
-	if i in 
-
-		if words[in_file_index] in candidates:
-			if re.match(match_pattern, words[match_index]): # re.match(pattern, string)
-				words[match_index] = change_to
-				changed_num += 1
-				out_file_h.write('\t'.join(words))
-			else:
-				unchanged_num += 1
-				out_file_h.write(line)
-				print(words[in_file_index], words[match_index])
-			candidates.remove(words[in_file_index])
-		else:
-			out_file_h.write(line)
+for i in init_snp_list:
+	out_file_h.write(i + '\t' + '\t'.join(out_dict[int(i)]) + '\n')
 out_file_h.close()
 
-# print candidates not in in_file
-if candidates:
-	print(str(len(candidates)), 'items not in in_info: ') 
-	print('\n'.join(candidates))
-
 # print report
-print('count_in = ', str(count_candidate), ', count_changed = ', str(changed_num), ', count_unchanged = ', str(unchanged_num))
+print('count_in = %i, in_cds = %i, in_pseudo = %i, in_other_genes = %i, intergenic = %i' % (len(init_snp_list), count_cds, count_pseudo, count_others, count_intergenic))
+
 # end of script
-
-
-
-count_candidate = len(candidates)
