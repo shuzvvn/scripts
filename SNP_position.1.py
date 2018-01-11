fef6e1aac42ce13bede18a76020c72815b181b01
Shuz <shuzvvn@gmail.com>
2018-01-11 18:10:14 +0800

    Update SNP_position.1.py

--- SNP_position.1.py
+++ SNP_position.1.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
+# SNP_position.1.py
+
+# Shu-Ting Cho <vivianlily6@hotmail.com>
+# report SNP position, in inter/intragenic region
+# v1 2018/01/11
+
+# Usage: python3 /home/shutingcho/pyscript/SNP_position.1.py python3 /home/shutingcho/pyscript/SNP_position.1.py --in_list=/home/shutingcho/project/phyto39/phyto39.03/SNP.2.list --in_list_index=0 --cds_info=/scratch/shutingcho/phyto39/phyto39.03/source_data/cds/CP000061.info --pseudo_info=/scratch/shutingcho/phyto39/phyto39.03/source_data/pseudo/CP000061.info --other_info=/scratch/shutingcho/phyto39/phyto39.03/source_data/RNA/CP000061.RNA --out_file=/home/shutingcho/project/phyto39/phyto39.03/SNP.CP000061.out
+
 import sys, getopt, re
 
-# Usage: python3 /home/shutingcho/pyscript/SNP_position.1.py \
-# --in_list=/home/shutingcho/project/phyto39/phyto39.03/SNP.2.list \
-# --in_list_index=0 \
-# --cds_info=/scratch/shutingcho/phyto39/phyto39.03/source_data/cds/NC_007716.info \
-# --pseudo_info=/scratch/shutingcho/phyto39/phyto39.03/source_data/rRNA/NC_007716.rRNA \
-# --other_info=/scratch/shutingcho/phyto39/phyto39.03/source_data/tRNA/NC_007716.tRNA \
-# --out_file=/home/shutingcho/project/phyto39/phyto39.03/SNP.2.out
-
+# get options
 opts, args = getopt.getopt(sys.argv[1:], '', longopts=[
 	'in_list=',
 	'in_list_index=',
@@ -35,7 +36,7 @@ for opt, arg in opts:
 	else:
 		assert False, "unhandled option"
 
-# make function to get list
+# function to get list
 def get_list_from_table(in_file, in_index = 0):
 	out_list = []
 	in_index = int(in_index)
@@ -45,6 +46,7 @@ def get_list_from_table(in_file, in_index = 0):
 			out_list.append(words[in_index])
 	return out_list
 
+# function to get dict
 def get_range_from_info(in_file, start_index, name_index):
 	out_dict = {}
 	with open(in_file, 'r') as in_file_h:
@@ -55,7 +57,6 @@ def get_range_from_info(in_file, start_index, name_index):
 
 # get list of SNP
 snp_list = get_list_from_table(in_list, in_list_index)
-init_snp_list = get_list_from_table(in_list, in_list_index)
 
 # get list of feature ranges
 feature_lists = []
@@ -76,13 +77,16 @@ out_dict = {}
 count_cds = 0
 count_pseudo = 0
 count_others = 0
+intragenic_list = []
 
 # intragenic SNP
-for i in snp_list: # each SNP position
-	i = int(i)
-	for feature_ranges in feature_lists: # each info dict
-		for feature_range in feature_ranges: # each range in dict
+for feature_ranges in feature_lists: # each info dict
+	for feature_range in feature_ranges: # each range in dict
+		snp_in_range = 0 # init snp in this range
+		for i in snp_list:
+			i = int(i)
 			if i in feature_range:
+				snp_in_range += 1
 				if feature_ranges == cds_ranges:
 					feature_type = 'cds'
 					count_cds += 1
@@ -93,23 +97,27 @@ for i in snp_list: # each SNP position
 					feature_type = 'others'
 					count_others += 1
 				out_dict[i] = [feature_type] + feature_ranges[feature_range]
-				snp_list.remove(str(i))
+				intragenic_list.append(str(i))
+			else:
+				if snp_in_range != 0:
+					break
 
 # intergenic SNP
-if snp_list:
-	count_intergenic = len(snp_list)
+intergenic_list = [x for x in snp_list if x not in intragenic_list]
+if intergenic_list:
+	count_intergenic = len(intergenic_list)
 	feature_type = 'intergenic'
-	for i in snp_list:
+	for i in intergenic_list:
 		i = int(i)
 		out_dict[i] = [feature_type]
 
 # write output
 out_file_h = open(out_file, 'w')
-for i in init_snp_list:
+for i in snp_list:
 	out_file_h.write(i + '\t' + '\t'.join(out_dict[int(i)]) + '\n')
 out_file_h.close()
 
 # print report
-print('count_in = %i, count_cds = %i, count_pseudo = %i, count_others = %i, intergenic = %i' % (len(init_snp_list), count_cds, count_pseudo, count_others, count_intergenic))
+print('count_SNP = %i\ncount_in_cds = %i\ncount_in_pseudo = %i\ncount_in_others = %i\ncount_in_intergenic = %i' % (len(snp_list), count_cds, count_pseudo, count_others, count_intergenic))
 
 # end of script
